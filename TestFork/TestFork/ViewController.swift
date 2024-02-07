21d59e8b08071bb576909b169a7e0c468e935640
Leo Shaxb <shaxbazyan001@mail.ru>
2024-02-07 19:27:21 +0400

    Initial commit

---
+++ TestFork/TestFork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFork
+//
+//  Created by Levon Shaxbazyan on 07.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
